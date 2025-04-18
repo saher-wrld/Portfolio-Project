8fa8d1a1af242762bd89faa9bf647cf63613aa9e
Saher Shaikh <sleepysaher@Sahers-MacBook-Air.local>
2025-04-17 20:45:03 -0400

    finishing changes of header and portfolio

--- portfolio/src/components/nav.jsx
+++ portfolio/src/components/nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './nav.css'; // import the CSS styles
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import name from '../../public/name.png'
 
@@ -10,6 +10,10 @@ import name from '../../public/name.png'
 const Header = () => {
 
 const navigate = useNavigate(); // Initialize the navigate function
+
+const location = useLocation();
+
+
 
 const handleAboutClick = () => {
   navigate('/about');
@@ -33,11 +37,34 @@ const handleContactClick = () => {
             <img src={name} alt="Logo" className="logo" />
           </div>
         <nav className="nav-links">
-            <p onClick={handleAboutClick} style={{ cursor: 'pointer' }}>about me</p>
-            <p onClick={handlePortfolioClick} style={{ cursor: 'pointer' }}>portfolio</p>
-            <p onClick={handleResumeClick} style={{ cursor: 'pointer' }}>resume</p>
-            <p onClick={handleContactClick} style={{ cursor: 'pointer' }}>contact</p>
-
+        <p
+  onClick={handleAboutClick}
+  className={location.pathname === '/about' ? 'active-link' : ''}
+  style={{ cursor: 'pointer' }}
+>
+  about me
+</p>
+<p
+  onClick={handlePortfolioClick}
+  className={location.pathname === '/portfolio' ? 'active-link' : ''}
+  style={{ cursor: 'pointer' }}
+>
+  portfolio
+</p>
+<p
+  onClick={handleResumeClick}
+  className={location.pathname === '/resume' ? 'active-link' : ''}
+  style={{ cursor: 'pointer' }}
+>
+  resume
+</p>
+<p
+  onClick={handleContactClick}
+  className={location.pathname === '/contact' ? 'active-link' : ''}
+  style={{ cursor: 'pointer' }}
+>
+  contact
+</p>
         </nav>
       </header>
     );
--- portfolio/src/pages/Portfolio.jsx
+++ portfolio/src/pages/Portfolio.jsx
@@ -9,37 +9,43 @@ function Portfolio() {
       name: 'FIZZ',
       repo: 'https://github.com/Treevyy/Fizz',
       link: 'https://github.com/Treevyy/Fizz',
-      description: 'Dating Website'
+      description: 'Dating Website',
+      imgName: 'fizz'
     },
     {
-      name: 'employeeTracker',
+      name: 'Employee Tracker',
       repo: 'https://github.com/saher-wrld/SQL-Employee-Tracker',
       link: 'https://github.com/saher-wrld/SQL-Employee-Tracker',
-      description: 'Postgres SQL database'
+      description: 'Postgres SQL database',
+      imgName: 'employeeTracker'
     },
     {
-      name: 'readmeGenerator',
+      name: 'README Generator',
       repo: 'https://github.com/saher-wrld/README_Generator',
       link: 'https://github.com/saher-wrld/README_Generator',
-      description: 'Command-line tool for auto-generating READMEs'
+      description: 'Command-line tool for auto-generating READMEs',
+      imgName: 'readmeGenerator'
     },
     {
-      name: 'vehicleBuilder',
+      name: 'Vehicle Builder',
       repo: 'https://github.com/saher-wrld/Vehicle_Builder',
       link: 'https://github.com/saher-wrld/Vehicle_Builder',
-      description: 'A vehicle builder project'
+      description: 'A vehicle builder project using typescript and inquirer',
+      imgName: 'vehicleBuilder'
     },
     {
-      name: 'memory',
+      name: 'Memory Game',
       repo: 'https://github.com/LPerez21/TestyourMemory',
       link: 'https://lperez21.github.io/TestyourMemory/',
-      description: 'Memory matching game'
+      description: 'Memory matching game',
+      imgName: 'memory'
     },
     {
-      name: 'portfolio',
+      name: 'Portfolio',
       repo: 'https://github.com/saher-wrld/Portfolio-Project',
       link: 'https://github.com/saher-wrld/Portfolio-Project',
-      description: 'My portfolio website' 
+      description: 'My portfolio website' ,
+      imgName: 'portfolio'
     }
   ]);
 
